f58833241df66af726e5f5960f8f3520e195a741
CodingWithRahmaan <abdurrehmanbasra1@gmail.com>
2025-04-10 02:08:58 +0400

    ....

--- app.js
+++ app.js
@@ -1,13 +1,71 @@
+const alarmSound = new Audio('asset/aaaaa.wav');
+let aa = document.getElementById("aa");
+let alarmTime 
+let aed
+let ee = document.getElementById("ee");
 
 
 
-  setInterval(function() {
+
+
+setInterval(function () {
     let abc = document.getElementById("abc");
-let time = moment().format(' h:mm:ss a')
-abc.innerHTML = time;
+    let currentTime = moment().format('HH:mm');
+    abc.innerHTML = moment().format('h:mm:ss a'); 
 
-let alarmTime = document.getElementById("alarmTime").value;
-if (alarmTime && time === alarmTime) {
-    alert("Alarm ringing!");
-}
+    if (alarmTime && currentTime === alarmTime && !aed) {
+        alarmSound.play(); 
+        alarmTriggered = true;
+    }
+
+    if (currentTime !== aed) {
+        alarmTriggered = false;
+    }
 }, 1000);
+
+
+
+
+function setAlarm() {
+    alarmTime = document.getElementById("alarmTime").value; 
+    if (alarmTime) {
+        Swal.fire({
+            title: 'Alarm Set!',
+            text: 'The alarm has been set.',
+            icon: 'success',
+            confirmButtonText: 'OK'
+          })
+        aed = false; 
+    } else {
+        Swal.fire({
+            title: 'Alarm!',
+            text: 'Please set the alarm first ⏰',
+            icon: 'warning',
+            confirmButtonText: 'OK'
+          });    
+    }
+}
+
+
+function stopAlarm() {
+    if(alarmTime){
+            alarmSound.pause(); 
+    aed = false;
+    alarmTime = null;
+    document.getElementById("alarmTime").value = ""; 
+    Swal.fire({
+        title: 'Alarm Stopped!',
+        text: 'The alarm has been stopped.',
+        icon: 'success',  
+        confirmButtonText: 'OK'
+      });
+    }
+    else {
+        Swal.fire({
+            title: 'Alarm!',
+            text: 'Please set the alarm first ⏰',
+            icon: 'warning',
+            confirmButtonText: 'OK'
+          });    
+    }   
+}
